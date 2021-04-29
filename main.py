77d5b7ca7ec55b7ed8d286c75952e67b72d8d170
JanJon5000 <jan@ipoczta.pl>
2021-04-29 23:45:15 +0200

    _

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pprint import pprint
 import chessBoardClass
+from usefullFunctions import setUptheBoard
 pygame.init()
 
 chessBoard = chessBoardClass.chessBoard()
@@ -8,7 +9,10 @@ pprint(chessBoard.boardModel)
 screen = pygame.display.set_mode((400, 400))
 
 running = True
+
+setUptheBoard(chessBoard, screen)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    pygame.display.update()
--- usefullFunctions.py
+++ usefullFunctions.py
@@ -7,4 +7,9 @@ def setUptheBoard(chessBoard: chessBoardClass.chessBoard, screen):
     screen = pygame.display.set_mode((400, 400))
     pygame.display.set_caption('Chess')
     for row in range(chessBoard.width):
-        
+        for column in range(chessBoard.height):
+            sqr = pygame.Rect(row*50, column*50, 50, 50)
+            if row % 2 == 0:
+                pygame.draw.rect(screen, brown, sqr)
+            else:
+                pygame.draw.rect(screen, white, sqr)
