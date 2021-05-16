6c51e6d08b6e39465f143d16b69e30296936a744
JanJon5000 <jan@ipoczta.pl>
2021-05-16 23:44:01 +0200

    small changes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from pprint import pprint
+from math import ceil
+from pygame.constants import MOUSEBUTTONDOWN
 import chessBoardClass
 from usefullFunctions import setUptheBoard
 pygame.init()
@@ -9,7 +11,6 @@ pieces = ['whitePawn', 'blackPawn', 'whiteRook', 'blackRook', 'whiteKnight', 'bl
           
 listOfSquares = []
 chessBoard = chessBoardClass.chessBoard()
-#pprint(chessBoard.boardModel)
 screen = pygame.display.set_mode((400, 400))
 
 running = True
@@ -19,4 +20,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_possition = pygame.mouse.get_pos()
+            for button in listOfSquares:
+                pass
+        
     pygame.display.update()
