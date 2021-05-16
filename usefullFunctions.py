828e54c231e1b4399484058e6818b2923519af56
JanJon5000 <jan@ipoczta.pl>
2021-05-16 23:23:56 +0200

    new Pieces and bigger board

--- usefullFunctions.py
+++ usefullFunctions.py
@@ -4,17 +4,18 @@ import pygame
 pygame.init()
 
 def setUptheBoard(chessBoard: chessBoardClass.chessBoard, screen, boardPosition: list, listOfSquares: list):
-    screen = pygame.display.set_mode((400, 400))
+    screen = pygame.display.set_mode((800, 800))
+    squareSize = 100
     pygame.display.set_caption('Chess')
     for row in range(chessBoard.width):
         for column in range(chessBoard.height):
-            sqr = pygame.Rect(row*50, column*50, 50, 50)
+            sqr = pygame.Rect(row*squareSize, column*squareSize, squareSize, squareSize)
             listOfSquares.append(sqr)
             if row % 2 == 0:
                 if column % 2 != 0:
                     pygame.draw.rect(screen, brown, sqr)
                 else:
-                    pygame.draw.rect(screen, white, sqr)___
+                    pygame.draw.rect(screen, white, sqr)
             else:
                 if column % 2 != 0:
                     pygame.draw.rect(screen, white, sqr)
@@ -24,6 +25,7 @@ def setUptheBoard(chessBoard: chessBoardClass.chessBoard, screen, boardPosition:
     for column in range(len(boardPosition)):
         for row in range(len(boardPosition[column])):
             for square in listOfSquares:
-                if square.centerx == 25+column*50 and square.centery == 25+row*50 and boardPosition[column][row] != None:
+                if square.centerx == squareSize/2+column*squareSize and square.centery == squareSize/2+row*squareSize and boardPosition[column][row] != None:
                     img = pygame.image.load(f'C:\\VSCODE\\projekt pygame\\chess\\img\\{boardPosition[column][row]}.png')
-                    screen.blit(img, (row*50+15, column*50+10))
+                    img = pygame.transform.smoothscale(img, (squareSize, squareSize))
+                    screen.blit(img, (row*squareSize, column*squareSize))
